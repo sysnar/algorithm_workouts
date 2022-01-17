d78a013784956e92a75fa7116894947322c0b6db
sysnar <sysn4r@gmail.com>
2022-01-17 21:54:47 +0900

    ADD chunk new solution

--- src/udemy/bootcamp/array-chunk/index.js
+++ src/udemy/bootcamp/array-chunk/index.js
@@ -1,14 +1,28 @@
+// Solution #1
+// export function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// Solution #2
 export function chunk(array, size) {
   const chunked = [];
+  let index = 0;
 
-  for (let element of array) {
-    const last = chunked[chunked.length - 1];
-
-    if (!last || last.length === size) {
-      chunked.push([element]);
-    } else {
-      last.push(element);
-    }
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
