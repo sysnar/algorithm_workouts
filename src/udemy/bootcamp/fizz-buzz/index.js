9b499b07966527df6c5cb42aa6cb301cec36079a
sysnar <sysn4r@gmail.com>
2022-01-17 21:43:15 +0900

    ADD fizz-buzz code

---
+++ src/udemy/bootcamp/fizz-buzz/index.js
@@ -0,0 +1,14 @@
+export function fizzBuzz(number) {
+  for (let i = 1; i <= number; i++) {
+    // if (i % 15 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
