5ed15ad525d3d4eef6f35c518451dcc719ce1df1
sysnar <sysn4r@gmail.com>
2022-01-20 02:08:43 +0900

    ADD fromlast code/test

---
+++ src/udemy/bootcamp/21-fromlast/index.js
@@ -0,0 +1,16 @@
+export function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
