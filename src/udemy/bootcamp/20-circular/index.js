3407e0e090f5d1a640053391e4d092458b5da6a2
sysnar <sysn4r@gmail.com>
2022-01-20 01:59:27 +0900

    ADD circular code / test

---
+++ src/udemy/bootcamp/20-circular/index.js
@@ -0,0 +1,15 @@
+export function circular(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
