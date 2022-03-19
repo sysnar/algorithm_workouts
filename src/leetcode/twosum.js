49205b3059303e6b2fd6468a63ec4e6b5ac2353e
sysnar <sysn4r@gmail.com>
2022-03-19 23:07:07 +0900

    add solved anwer

---
+++ src/leetcode/twosum.js
@@ -0,0 +1,15 @@
+const twoSum = (nums, target) => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in map) {
+      return [map[another], i];
+    }
+
+    map[nums[i]] = i;
+  }
+
+  return null;
+};
