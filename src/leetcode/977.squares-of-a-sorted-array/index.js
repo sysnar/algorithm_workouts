de4706783869e87ba82effdf4a5a4519af3b8930
sysnar <sysn4r@gmail.com>
2022-03-28 23:03:19 +0900

    new solution

--- src/leetcode/977.squares-of-a-sorted-array/index.js
+++ src/leetcode/977.squares-of-a-sorted-array/index.js
@@ -2,17 +2,52 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0) nums[i] *= -1;
+// Solution #1
+// var sortedSquares = function (nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     nums[i] *= nums[i];
+//   }
+//   nums.sort((a, b) => a - b);
+
+//   return nums;
+// };
+
+// Solution #2 -> Push Array Hight to Low
+// export var sortedSquares = function (nums) {
+// let length = nums.length;
+// let result = [];
+// let left = 0;
+// let right = length - 1;
+
+// while (left <= right) {
+//   if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+//     result.unshift(nums[right] * nums[right]);
+//     right--;
+//   } else {
+//     result.unshift(nums[left] * nums[left]);
+//     left++;
+//   }
+// }
+
+// return result;
+// };
+
+// Solution #2 -> Push Array Hight to Low
+export var sortedSquares = function (nums) {
+  let length = nums.length;
+  let result = new Array(length);
+  let left = 0;
+  let right = length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      result[i] = nums[right] ** 2;
+      right--;
+    } else {
+      result[i] = nums[left] ** 2;
+      left++;
+    }
   }
 
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    nums[i] *= nums[i];
-  }
-
-  return nums;
+  return result;
 };
-
-console.log(sortedSquares([-7, -3, 2, 3, 11]));
