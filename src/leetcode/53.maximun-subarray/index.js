4041d247d1598617f947280aa6eb01739a9d146f
sysnar <sysn4r@gmail.com>
2022-06-16 20:42:40 +0900

    Add leetcode 53 problem solve with Kadane Algorithm

--- src/leetcode/53.maximun-subarray/index.js
+++ src/leetcode/53.maximun-subarray/index.js
@@ -1,6 +1,20 @@
-export const maxSubArray = function (nums) {
+const maxSubArray2 = function (nums) {
   for (let index = 1; index < nums.length; index++) {
     nums[index] += nums[index - 1] > 0 ? nums[index - 1] : 0;
   }
   return Math.max(...nums);
+};
+
+// Kadane Algorithm
+// 아래 사항은 카데인 알고리즘을 적용해본 예시입니다.
+export const maxSubArray = function (nums) {
+  let sum = 0;
+  let max = -Math.max(...nums);
+
+  for (let num of nums) {
+    sum = Math.max(num, num + sum);
+    max = Math.max(sum, max);
+  }
+
+  return max;
 };
